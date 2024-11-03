62869cd5391ed248de6ebdfe896e73cf30e19225
BugsBountyHunter <ahmed.saber20170200@gmail.com>
2024-11-03 12:52:40 +0200

    update: mysql db

--- bootstrap.php
+++ bootstrap.php
@@ -9,7 +9,7 @@ $container = new Container();
 // register in container
 $container->bind(Database::class, function () {
   $config = require base_path("config.php");
-  return new Database($config["database"], "root", "Ahmed@Saber_1996$");
+  return new Database($config["database"], "root", "123456_@QWE");
 });
 
 App::setContainer($container);
